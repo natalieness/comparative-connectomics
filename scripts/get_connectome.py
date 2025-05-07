fca8bb3e908ce35981b5699777d1e790db7ad25a
natalieness <natalie.ness@outlook.com>
2025-05-07 09:45:07 +0100

    added script to load existing connectome

---
+++ scripts/get_connectome.py
@@ -0,0 +1,29 @@
+
+import os
+
+import networkx as nx
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from contools import Celltype, Celltype_Analyzer, Promat
+import pymaid
+from pymaid_creds import url, name, password, token
+
+rm = pymaid.CatmaidInstance(url, token, name, password)
+
+#get parent directory path
+current_file = __file__  
+current_dir = os.path.dirname(current_file)
+parent_dir = os.path.dirname(current_dir)
+path_for_data = parent_dir+'/data/'
+
+#%%
+# select neurons to include 
+wanted_neurons = pymaid.get_skids_by_annotation(['mw brain and inputs', 'mw brain accessory neurons'])
+remove_neurons = pymaid.get_skids_by_annotation(['mw brain very incomplete', 'mw partially differentiated', 'mw motor'])
+all_neurons = list(np.setdiff1d(wanted_neurons, remove_neurons)) # remove neurons that are incomplete or partially differentiated (as well as SEZ motor neurons)
+
+# get all synaptic sites associated with neurons 
+links = pymaid.get_connector_links(all_neurons, chunk_size=50)
