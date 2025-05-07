c757436828584bfbb2ebf17da8ba7bf4bc57f6a8
natalieness <natalie.ness@outlook.com>
2025-05-07 12:13:33 +0100

    added neuron manipulation functions

--- scripts/get_connectome.py
+++ scripts/get_connectome.py
@@ -2,6 +2,7 @@
 import os
 from dataclasses import dataclass, asdict
 from typing import List, Tuple, Dict, Union, Any
+import pickle
 
 import networkx as nx
 import numpy as np
@@ -15,6 +16,7 @@ from pymaid_creds import url, name, password, token
 
 from scripts.nx_graph_functions import build_adj_directed_graph, plot_nx_digraph
 from scripts.change_class import ChangeLog
+from scripts.network_manipulation_functions import generate_mirror_network, neuron_duplication, neuron_deletion
 
 rm = pymaid.CatmaidInstance(url, token, name, password)
 
@@ -53,9 +55,7 @@ seed = 42
 #generate numpy random instance
 rng = np.random.default_rng(seed=seed)
 
-def generate_mirror_network(adj_):
-    adj_mirror = adj_.copy()
-    return adj_mirror
+
 
 adj_mirror = generate_mirror_network(adj)
 #%% class instance to save matrix changes 
@@ -64,87 +64,23 @@ adj_mirror = generate_mirror_network(adj)
 # %%
 log = ChangeLog()
 
-def neuron_duplication(adj_, log, n_ops=1):
-    new_adj = adj_.copy()
-    #calling n_neurons here prevents duplication of new neurons
-    n_neurons = new_adj.shape[0]
-    for n in range(n_ops):
-        # select a random neuron idx
-        neuron_idx = rng.integers(low=0, high=n_neurons, size=1)[0]
-        # get the neuron name
-        neuron_name = new_adj.columns[neuron_idx]
-
-        #get neuron outputs 
-        n_source = new_adj.iloc[neuron_idx, :].values
-        syn_out = np.where(n_source > 0)[0]
-        w_out = n_source[syn_out]
-        new_w_out = rng.shuffle(w_out)
-        #set new weights 
-        new_n_source = n_source.copy()
-        new_n_source[syn_out] = new_w_out
-
-        #set new neuron name
-        new_n_name = f'new_{n}'
-        new_adj.loc[new_n_name] = new_n_source
-
-        #get neuron inputs
-        n_target = new_adj.iloc[:, neuron_idx].values
-        n_in = np.where(n_target > 0)[0]
-        w_in = n_target[n_in]
-        new_w_in = rng.shuffle(w_in)
-        #set new weights
-        new_n_target = n_target.copy()
-        new_n_target[n_in] = new_w_in
-    
-        new_adj[new_n_name] = new_n_target
-
-        log.add_neuron_change(
-            operation='neuron_duplication',
-            source_index=neuron_name,
-            new_index=new_n_name,
-            weight_handling='shuffling existing'
-        )
-
-    return new_adj, log
-
-def neuron_deletion(adj_, log, n_ops=1):
-    new_adj = adj_.copy()
-    n_neurons = new_adj.shape[0]
-    for n in range(n_ops):
-        # select a random neuron idx
-        neuron_idx = rng.integers(low=0, high=n_neurons, size=1)[0]
-        # get the neuron name
-        neuron_name = new_adj.columns[neuron_idx]
-
-        # delete the neuron from the adjacency matrix
-        new_adj.drop(columns=[neuron_name], inplace=True)
-        new_adj.drop(index=[neuron_name], inplace=True)
-
-        log.add_neuron_change(
-            operation='neuron_deletion',
-            source_index=neuron_name,
-            new_index=None,
-            weight_handling='NA'
-        )
-
-    return new_adj, log
-
-
+# neuron-level manipulations
 n_ops_neuron = 30
 adj_mirror, log = neuron_duplication(adj_mirror, log, n_ops=n_ops_neuron)
 adj_mirror, log = neuron_deletion(adj_mirror, log, n_ops=n_ops_neuron)
 
 
-
-
-
-
-
 # %% save altered adjacency matrix and change log 
 
+#sanity check to see that changes have been made 
+print(log)
 #save new adjacency matrix
 adj_mirror.to_csv(path_for_data+'adj_mirror.csv')
 
-print(log)
+#save change log class instance
+with open(path_for_data+'change_log.pkl', 'wb') as f:
+    pickle.dump(log, f)
+
+print('adjancey matrix and log saved')
 
 # %%
---
+++ scripts/network_manipulation_functions.py
@@ -0,0 +1,76 @@
+''' Functions to generate an artificially manipulated connectome from 
+an existing connectome.'''
+
+### Basics ###
+
+def generate_mirror_network(adj_):
+    adj_mirror = adj_.copy()
+    return adj_mirror
+
+### Neuron-level manipulations ###
+
+
+def neuron_duplication(adj_, log, n_ops=1):
+    new_adj = adj_.copy()
+    #calling n_neurons here prevents duplication of new neurons
+    n_neurons = new_adj.shape[0]
+    for n in range(n_ops):
+        # select a random neuron idx
+        neuron_idx = rng.integers(low=0, high=n_neurons, size=1)[0]
+        # get the neuron name
+        neuron_name = new_adj.columns[neuron_idx]
+
+        #get neuron outputs 
+        n_source = new_adj.iloc[neuron_idx, :].values
+        syn_out = np.where(n_source > 0)[0]
+        w_out = n_source[syn_out]
+        new_w_out = rng.shuffle(w_out)
+        #set new weights 
+        new_n_source = n_source.copy()
+        new_n_source[syn_out] = new_w_out
+
+        #set new neuron name
+        new_n_name = f'new_{n}'
+        new_adj.loc[new_n_name] = new_n_source
+
+        #get neuron inputs
+        n_target = new_adj.iloc[:, neuron_idx].values
+        n_in = np.where(n_target > 0)[0]
+        w_in = n_target[n_in]
+        new_w_in = rng.shuffle(w_in)
+        #set new weights
+        new_n_target = n_target.copy()
+        new_n_target[n_in] = new_w_in
+    
+        new_adj[new_n_name] = new_n_target
+
+        log.add_neuron_change(
+            operation='neuron_duplication',
+            source_index=neuron_name,
+            new_index=new_n_name,
+            weight_handling='shuffling existing'
+        )
+
+    return new_adj, log
+
+def neuron_deletion(adj_, log, n_ops=1):
+    new_adj = adj_.copy()
+    n_neurons = new_adj.shape[0]
+    for n in range(n_ops):
+        # select a random neuron idx
+        neuron_idx = rng.integers(low=0, high=n_neurons, size=1)[0]
+        # get the neuron name
+        neuron_name = new_adj.columns[neuron_idx]
+
+        # delete the neuron from the adjacency matrix
+        new_adj.drop(columns=[neuron_name], inplace=True)
+        new_adj.drop(index=[neuron_name], inplace=True)
+
+        log.add_neuron_change(
+            operation='neuron_deletion',
+            source_index=neuron_name,
+            new_index=None,
+            weight_handling='NA'
+        )
+
+    return new_adj, log
