335db6d0e48b5af9b966c73e765f5b574e323f65
natalieness <natalie.ness@outlook.com>
2025-05-09 11:27:15 +0100

    added adj filtered by paired neurons and dict to look up pairs data

--- generate_connectomes/trialling_neuron_matching.py
+++ generate_connectomes/trialling_neuron_matching.py
@@ -75,5 +75,56 @@ for row in range(pairs_data.shape[0]):
 
 n_bilateral_skids_in_adj = len(bilateral_skids_in_adj)
 print(f"Number of pairs in adjacency matrix: {n_bilateral_skids_in_adj}, total number of neurons: {n_bilateral_skids_in_adj*2} out of {n_adj_ids}")
+bilateral_skids_in_adj_flat = np.unique(np.array(list(chain.from_iterable(bilateral_skids_in_adj))))
+
+
+# %% filter adj by bilateral skids 
+
+## to do in easiest way possible and add to main script getting adj matrix 
+
+#%% create dictionary to access skid metadata (left/right, region, and matching skid)
+
+def create_pairs_dict(pairs_data, verbose=False):
+    pairs_df = pd.DataFrame()
+    pairs_df['neuron_id'] = pairs_data['leftid'].values
+    pairs_df['side'] = 'left'
+    pairs_df['region'] = pairs_data['region'].values
+    pairs_df['partner'] = pairs_data['rightid'].values
+
+    pairs_df2 = pd.DataFrame()
+    pairs_df2['neuron_id'] = pairs_data['rightid'].values
+    pairs_df2['side'] = 'right'
+    pairs_df2['region'] = pairs_data['region'].values
+    pairs_df2['partner'] = pairs_data['leftid'].values
+
+    # merge the two dataframes 
+    pairs_df = pd.concat((pairs_df, pairs_df2), axis=0)
+    pairs_df = pairs_df.reset_index(drop=True)
+    #set partners to list 
+    pairs_df['partner'] = pairs_df['partner'].apply(lambda x: [x])
+
+    # handle duplicates 
+    non_unique = np.unique(pairs_df['neuron_id'].values[pairs_df['neuron_id'].duplicated(keep=False)])
+
+    if verbose:
+        print(f"Number of non-unique neurons: {len(non_unique)}")
+    for n in non_unique: 
+        occs = np.where(pairs_df['neuron_id'].values == n)[0]
+        partners = pairs_df['partner'].values[occs]
+        #add to the first occurance of the neuron 
+
+       #print(list(chain.from_iterable(partners))
+        partners_ = list(chain.from_iterable(partners))
+ 
+        pairs_df.at[occs[0], 'partner'] = partners_
+        #remove the rest
+        pairs_df = pairs_df.drop(occs[1:], axis=0)
+        pairs_df = pairs_df.reset_index(drop=True)
+
+    pairs_df = pairs_df.set_index('neuron_id')
+    pairs_dict = pairs_df.to_dict(orient='index')
+    return pairs_dict
+
+pairs_dict = create_pairs_dict(pairs_data)
 
 # %%
