1a3d37030790d4b3d283a6252a9ced7a94f8739c
natalieness <natalie.ness@outlook.com>
2025-06-02 16:11:36 +0100

    added adj heatmaps

--- analysis_scripts/compare_connectomes.py
+++ analysis_scripts/compare_connectomes.py
@@ -10,6 +10,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from graspologic.inference.density_test import density_test
+from graspologic.simulations import er_np
+from graspologic.plot import heatmap
+from graspologic.utils import binarize
 from contools import Celltype, Celltype_Analyzer, Promat
 import pymaid
 from pymaid_creds import url, name, password, token
@@ -40,14 +44,7 @@ adj2 = adj2.values
 
 
 # %% density 
-from graspologic.inference.density_test import density_test
-from graspologic.simulations import er_np
-from graspologic.plot import heatmap
-
 np.random.seed(42)
-
-
-
 stat, pval, misc = density_test(adj1, adj2, method='fisher')
 print(f'Comparison with generated mirror network, pvalue = {pval}')
 
@@ -57,4 +54,9 @@ random_adj = er_np(n=adj1.shape[0], p=p, directed=True)
 stat_random, pval_random, misc_random = density_test(adj1, random_adj, method='fisher')
 print(f'Comparison with random network of same size with probability {p}, pvalue = {pval_random}')
 
+# %% plot binarized adjacency matrices
+adj1_bi = binarize(adj1)
+adj2_bi = binarize(adj2)
+heatmap(adj1_bi, title='Binarized Adjacency Matrix 1')
+heatmap(adj2_bi, title='Binarized Adjacency Matrix 2')
 # %%
