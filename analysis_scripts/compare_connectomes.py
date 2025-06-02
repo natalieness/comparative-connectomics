7a25e69159a5c68d33b1c0c4205ca40634472078
natalieness <natalie.ness@outlook.com>
2025-06-02 15:57:10 +0100

    added density test

--- analysis_scripts/compare_connectomes.py
+++ analysis_scripts/compare_connectomes.py
@@ -23,12 +23,35 @@ parent_dir = os.path.dirname(current_dir)
 path_for_data = parent_dir+'/data/'
 
 #%% Load adjacency matrix
-adj1 = pd.read_csv(path_for_data+'adj.csv', index_col=0)
+adj1 = pd.read_csv(path_for_data+'b_adj.csv', index_col=0)
 print(f"Adjacency matrix 1 shape: {adj1.shape}")
 
-adj2 = pd.read_csv(path_for_data+'adj_mirror.csv', index_col=0)
+adj2 = pd.read_csv(path_for_data+'b_adj_mirror.csv', index_col=0)
 print(f"Adjacency matrix 2 shape: {adj2.shape}")
 
+# check for NaN values in adjacency matrices
+if adj1.isnull().values.any():
+    print("Warning: Adjacency matrix contains NaN values. This may cause issues in further analysis.")
+if adj2.isnull().values.any():
+    print("Warning: Adjacency matrix contains NaN values. This may cause issues in further analysis.")
+#%% 
+adj1 = adj1.values
+adj2 = adj2.values
+
+
+# %% density 
+from graspologic.inference.density_test import density_test
+from graspologic.simulations import er_np
+from graspologic.plot import heatmap
+
+np.random.seed(42)
+
+
+stat, pval, misc = density_test(adj1, adj2, method='fisher')
+
+#compare to random 
+random_adj = er_np(n=adj1.shape[0], p=0.01, directed=True)
+stat_random, pval_random, misc_random = density_test(adj1, random_adj, method='fisher')
+
+
 # %%
-
-
